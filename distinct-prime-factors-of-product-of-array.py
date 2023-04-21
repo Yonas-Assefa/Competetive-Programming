103c9cba1d22bad7c3e913250d20f149334e368b
Yonas-Assefa <61969031+Yonas-Assefa@users.noreply.github.com>
2023-04-21 21:36:51 +0200

    added distinct-prime-factors-of-product-of-array.python

---
+++ distinct-prime-factors-of-product-of-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def distinctPrimeFactors(self, nums: List[int]) -> int:
+        # prod = 1
+        # for i in nums:
+        #     prod *= i
+        # prime = 2
+
+        primeFactors = set()
+        for i in nums:
+            prod = i
+            prime = 2
+            while prime * prime <= prod:
+                
+                while  prod % prime == 0:
+                    prod = prod // prime
+                    primeFactors.add(prime)
+
+                prime += 1
+
+            if prod > 1:
+                primeFactors.add(prod)
+            # print(primeFactors)
+       
+        return len(primeFactors)
